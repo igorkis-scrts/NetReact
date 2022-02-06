35fecf87177e48e89a245f34800b59a2347fd35b
Igor Kiselyov <igorkiselyov74@live.com>
2022-02-06 16:48:58 +0300

    fixed wrong page (BookList)

--- src/BookExchange.React/src/app/components/Pages/Books/BookList/BookList.tsx
+++ src/BookExchange.React/src/app/components/Pages/Books/BookList/BookList.tsx
@@ -40,7 +40,7 @@ const BookList = (props: IBookListProps) => {
 
   const handleChangeRowsPerPage = (event: SelectChangeEvent) => {
     setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
+    setPage(1);
   };
 
   const handleAddBookClick = () => {
